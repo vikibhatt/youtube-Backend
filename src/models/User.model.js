ca8993097fad02f11161f4d7c3a33c2e6b58f5b9
Vikas bhatt <96935547+viki8392@users.noreply.github.com>
2024-01-08 16:40:34 +0530

    new changes on user model

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -47,7 +47,7 @@ const UserSchema = new mongoose.Schema({
 UserSchema.pre('save', async function (){
     if(!this.isModified("password")) return next()
 
-    this.password = bcrypt.hash(this.password,10)
+    this.password = await bcrypt.hash(this.password,10)
     next()
 })
 
