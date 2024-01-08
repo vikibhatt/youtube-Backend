aa6c7e7921d80c954b42895b8d4ab04e60ec7ca8
Vikas bhatt <96935547+viki8392@users.noreply.github.com>
2024-01-08 16:32:27 +0530

    added cloudnary and multer middleware

---
+++ src/utils/cloudnary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudnary} from 'cloudinary'
+import fs from 'fs'
+          
+cloudnary.config({ 
+  cloud_name: process.env.CLOUDNARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDNARY_CLOUD_API_KEY, 
+  api_secret: process.env.CLOUDNARY_CLOUD_API_SECRET 
+});
+
+
+const cloudnaryFileUpload = async(localFilePath) =>{
+    try {
+        if(!localFilePath) return null
+
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        })
+
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+
+export default cloudnaryFileUpload
