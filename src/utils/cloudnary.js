bb56b6f4b729ae17f0e79b1894465eae89cfe9b9
vikas <vikasbhatt739@gmail.com>
2024-02-04 21:19:57 +0530

    new changes

--- src/utils/cloudnary.js
+++ src/utils/cloudnary.js
@@ -1,5 +1,10 @@
 import {v2 as cloudnary} from 'cloudinary'
 import fs from 'fs'
+import dotenv from 'dotenv'
+
+dotenv.config({
+    path: './.env'
+})
           
 cloudnary.config({ 
   cloud_name: process.env.CLOUDNARY_CLOUD_NAME, 
